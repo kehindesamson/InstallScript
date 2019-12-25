2ee810002f0da42e1066f5c4ed7da55152da657d
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2019-12-25 14:28:24 +0100

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -17,7 +17,6 @@
 OE_USER="odoo"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
-# The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
@@ -174,7 +173,7 @@ NAME=$OE_CONFIG
 DESC=$OE_CONFIG
 # Specify the user name (Default: odoo).
 USER=$OE_USER
-# Specify an alternate config file (Default: /etc/openerp-server.conf).
+# Specify an alternate config file (Default: /etc/odoo-server.conf).
 CONFIGFILE="/etc/${OE_CONFIG}.conf"
 # pidfile
 PIDFILE=/var/run/\${NAME}.pid
